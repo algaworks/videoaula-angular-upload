189ff27837b9f7a8b8c9d8fb0c2fd72b85a64dce
Alexandre Afonso <afonsoaaf@gmail.com>
2018-05-18 12:59:19 -0300

    Videoaula gravada.

---
+++ videoaula-angular-upload-ui/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private http: HttpClient) {}
+
+  inputFileChange(event) {
+    if (event.target.files && event.target.files[0]) {
+      const foto = event.target.files[0];
+
+      const formData = new FormData();
+      formData.append('foto', foto);
+
+      this.http.post('http://localhost:8080/fotos', formData)
+        .subscribe(resposta => console.log('Upload ok.'));
+    }
+  }
+}
